30fef7c86341520cdc54160623e3285aeb304d9b
jerh011 <jeelrohe@gmail.com>
2023-11-20 21:01:08 -0700

    Pilas sin enlazar

---
+++ MangaStore!/Modelos/Mangas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaStore_.Modelos
+{
+    public class Mangas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int Tomo { get; set; }
+        public string Author { get; set; }
+        public string Editorial { get; set; }
+        public string Genereo { get; set; }
+        public double Precio { get; set; }
+    }
+}
