87946874ab78714d6b1f84e2bf85876c6d89d40a
jerh011 <jeelrohe@gmail.com>
2023-12-02 22:20:18 -0700

    Listas y arrreglos ya estan (creo)

---
+++ MangaStore!/Datos/Listas/IntermediarioListas.cs
@@ -0,0 +1,43 @@
+﻿using MangaStore_.Datos.Pilas;
+using MangaStore_.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaStore_.Datos.Listas
+{
+    internal class IntermediarioListas
+    {
+        private ListasLogica listas=new ListasLogica();
+        private Mangas[] manga;
+        private static IntermediarioListas _instancia;
+        public static IntermediarioListas Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new IntermediarioListas();
+                }
+                return _instancia;
+            }
+        }
+        public Mangas[] OptenerListas()
+        {
+             return manga = listas.OptenerListado();
+        }
+        public void insertarLista(Mangas manga)
+        {
+            listas.InsertaNodo(manga);
+        }
+        public void EditarLitas(Mangas manga) {
+            listas.Editar(manga);
+        }
+        public void EliminarLitas(int _ID)
+        {
+            listas.Eliminar(_ID);
+        }
+    }
+}
---
+++ MangaStore!/Datos/Listas/ListasLogica.cs
@@ -0,0 +1,97 @@
+﻿using MangaStore_.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaStore_.Datos.Listas
+{
+    public class ListasLogica
+    {
+        private Nodo _primero;
+        private Nodo _ultimo;
+
+        public ListasLogica()
+        {
+            _primero = null;
+            _ultimo = null;
+        }
+        public void InsertaNodo(Mangas manga)
+        {
+            Nodo nuevo = new Nodo(manga);
+            if (_primero == null)
+              _ultimo=_primero = nuevo;
+            else
+             _ultimo=_ultimo.Siguiente = nuevo;
+        }
+        public int Longitud() {
+            int cont = 0;
+            Nodo actual = _primero;
+            while (actual != null)
+            { 
+                cont ++;
+                actual = actual.Siguiente;
+            }
+            return cont;
+        }
+   
+        public void Editar(Mangas mangas)
+        {
+            Nodo _actual = _primero;
+         
+                while (_actual != null)
+                { 
+                    if (_actual.Dato.Id ==mangas.Id)
+                    {
+                        _actual.Dato = mangas;
+                        break;
+                    }
+                    _actual = _actual.Siguiente;
+                }
+          
+        }
+        public void Eliminar(int Idmanga)
+        {
+            Nodo actual = _primero;
+            Nodo anterior = null;
+            while (actual != null)
+            {
+                if (actual.Dato.Id == Idmanga)
+                {
+                    if (anterior == null)
+                    {
+                        _primero = actual.Siguiente;
+                        if (_primero == null)
+                            _ultimo = null;
+
+                    }
+                    else
+                    {
+                        anterior.Siguiente = actual.Siguiente;
+
+                        if (actual.Siguiente == null)
+                            _ultimo= anterior;
+                    }
+
+                    break;
+                }
+
+                anterior = actual;
+                actual = actual.Siguiente;
+            }
+        }
+        public Mangas[] OptenerListado()
+        {
+            Mangas[] mangas = new Mangas[Longitud()];
+            Nodo _actual = _primero;
+            
+                for (int i = 0; i < Longitud()&&_actual!=null; i++)
+                {
+                    mangas[i] = _actual.Dato;
+                    _actual = _actual.Siguiente;
+                }
+            return mangas;
+        }
+    }
+}
